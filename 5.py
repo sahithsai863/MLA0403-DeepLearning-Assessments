160a5d6c8d8898cd16c86585eb5cb57298a11121
sahithsai863 <192425029.simats@saveetha.com>
2026-07-25 17:05:06 +0530

    Add numpy operations for weighted sum and activations

---
+++ 5.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+x=np.array([1,2,3])
+
+w=np.array([0.5,0.2,0.8])
+
+z=np.dot(x,w)
+
+sigmoid=1/(1+np.exp(-z))
+
+relu=max(0,z)
+
+print("Weighted Sum:",z)
+print("Sigmoid:",sigmoid)
+print("ReLU:",relu)
